2946f1e3ca73c434f7646e862372ba57d5aa3200
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-08-16 17:49:25 +0400

    fix heading with Text component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./styles.css";
+import Text from "../Text";
 
 const Hero = () => {
   return (
     <section className="hero-container">
       <div className="hero-content">
-        <h2>Buliding Digital Experiences That Inspire</h2>
+        <Text
+          componentTag="h2"
+          className="hero-title"
+          size="3.8rem"
+          weight={600}
+        >
+          Buliding Digital Experiences That Inspire
+        </Text>
         <p>
           Passionate Frontend Developer | Transforming Ideas Into Seamless and
           Visually Stunning Web Solutions
@@ -17,17 +25,17 @@ const Hero = () => {
           <div className="tech-icon">
             <img src="./assets/images/img01.png" alt="" />
           </div>
-        {/* <img className="hero-pic" src="./assets/images/logo.svg" alt="" /> */}
+          {/* <img className="hero-pic" src="./assets/images/logo.svg" alt="" /> */}
         </div>
         <div className="tech-icon">
-            <img src="./assets/images/img02.png" alt="" />
-          </div>
-          <div className="tech-icon">
-            <img src="./assets/images/img03.png" alt="" />
-          </div>
-          <div className="tech-icon">
-            <img src="./assets/images/img04.png" alt="" />
-          </div>
+          <img src="./assets/images/img02.png" alt="" />
+        </div>
+        <div className="tech-icon">
+          <img src="./assets/images/img03.png" alt="" />
+        </div>
+        <div className="tech-icon">
+          <img src="./assets/images/img04.png" alt="" />
+        </div>
       </div>
     </section>
   );
