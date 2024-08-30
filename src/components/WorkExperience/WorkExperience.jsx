42820a8b144c73dba7c190bb3466a4ae10e963ae
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-08-30 09:26:14 +0400

    Creating react-slick theme

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./WorkExperience.css";
 import { WORK_EXPERIENCE } from "../../utils/data";
 import ExperienceCard from "./ExperienceCard/ExperienceCard";
+import Slider from "react-slick";
 
 const WorkExperience = () => {
+  const sliderRef = useRef();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToSHow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <section className="experience-container">
       <h5>Work Experience</h5>
 
-      <div className="experience-content"></div>
-      {WORK_EXPERIENCE.map((item) => (
-        <ExperienceCard key={item.title} details={item} />
-      )
-    )}
+      <div className="experience-content">
+        <Slider ref={sliderRef} {...settings}>
+          {WORK_EXPERIENCE.map((item) => (
+            <ExperienceCard key={item.title} details={item} />
+          ))}
+        </Slider>
+      </div>
     </section>
   );
 };
