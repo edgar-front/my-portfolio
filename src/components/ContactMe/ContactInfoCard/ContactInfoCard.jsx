b68ed8f60253dfd9d99a00eb2343082ef366d0b4
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-09-05 12:18:17 +0400

    create ContactMeInfoCard

---
+++ src/components/ContactMe/ContactInfoCard/ContactInfoCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ContactInfoCard.css";
+
+const ContactInfoCard = ({iconUrl, text}) => {
+  return <div>ContactInfoCard</div>;
+};
+
+export default ContactInfoCard;
