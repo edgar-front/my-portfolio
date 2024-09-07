11994bb3ccabdcaa2d2df06c6cbcbe14375dd934
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-09-07 11:14:26 +0400

    email and github icons styles

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./styles.css";
+import ContactInfoCard from "./ContactInfoCard/ContactInfoCard";
 
 const ContactMe = () => {
   return (
     <section className="contact-container">
       <h5>Contact Me</h5>
       <div className="contact-content">
-        <div style={{ flex: 1 }}></div>
+        <div style={{ flex: 1 }}>
+          <ContactInfoCard
+            iconUrl="./assets/images/email-icon.svg"
+            text="edgar.itmember@gmail.com"
+          />
+          <ContactInfoCard
+            iconUrl="./assets/images/github-icon.svg"
+            text="https://github.com/edgar-front/my-portfolio"
+          />
+        </div>
         <div style={{ flex: 1 }}></div>
       </div>
     </section>
