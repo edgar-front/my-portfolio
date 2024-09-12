99258ea852f4583484dfe1f9d842d633cfae87eb
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-09-12 15:30:48 +0400

    uptade mobilenav

--- src/components/Navbar/MobileNav/index.jsx
+++ src/components/Navbar/MobileNav/index.jsx
@@ -14,6 +14,18 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
             <li>
               <a className="menu-item">Home</a>
             </li>
+            <li>
+              <a className="menu-item">Skills</a>
+            </li>
+            <li>
+              <a className="menu-item">Work Experience</a>
+            </li>
+            <li>
+              <a className="menu-item">Contact Me</a>
+            </li>
+            <button className="contact-btn" onClick={() => {}}>
+              Hire Me
+            </button>
           </ul>
         </div>
       </div>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Text from "../Text";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState[false];
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <>
+      <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+
       <nav className="nav-wrapper">
         <div className="nav-content">
           <span>Time to program</span>
@@ -44,11 +52,9 @@ const Navbar = () => {
             </button>
           </ul>
 
-          <button class="menu-btn" onClick={() => {}}>
+          <button class="menu-btn" onClick={toggleMenu}>
             <div class="material-symbols-outlined">
-              <div></div>
-              <div></div>
-              <div></div>
+              {openMenu ? "close" : "menu"}
             </div>
           </button>
         </div>
