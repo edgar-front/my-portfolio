f2734d285b05536d094c7f5726aff02fc317f459
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-10-16 12:34:43 +0400

    uptade mobile-workexperience

--- src/components/WorkExperience/ExperienceCard/index.jsx
+++ src/components/WorkExperience/ExperienceCard/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles.css";
-
+import Text from "../../Text";
 const ExperienceCard = ({ details }) => {
   return (
     <div className="work-experience-card">
-      <h6>{details.title}</h6>
+      <Text size="15" componentTag="h5">
+        {details.title}
+      </Text>
 
       <div className="work-duration">{details.date}</div>
 
