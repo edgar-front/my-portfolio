1a6b01e44166bb12b9149e9096a4ea26164b7728
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-10-17 10:43:25 +0400

    uptade mobile-skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 import { SKILLS } from "../../utils/data";
 import SkillCard from "./SkillCard";
 import SkillsInfoCard from "./SkillsInfoCard";
-import Text from "../../Text";
+import Text from "../Text";
 
 const Skills = () => {
   const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
@@ -11,8 +11,10 @@ const Skills = () => {
     setSelectedSkill(data);
   };
   return (
-    <section className="skills-container">
-      <Text>Technical Proficiency</Text>
+    <section>
+      <Text size="20px" componentTag="h5" className="skills-container">
+        Technical Proficiency
+      </Text>
       <div className="skills-content">
         <div className="skills">
           {SKILLS.map((item) => (
