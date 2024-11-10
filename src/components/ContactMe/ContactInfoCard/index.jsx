40d7c78a5597a684fc900060ce5056d3db1ff05a
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-11-10 10:40:00 +0400

    uptade refactor

--- src/components/ContactMe/ContactInfoCard/index.jsx
+++ src/components/ContactMe/ContactInfoCard/index.jsx
@@ -18,6 +18,21 @@ const ContactInfoCard = ({ text, name }) => {
       <button onclick="myRefactor()">Copy text</button>
     </div>
   );
+  function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+    myFunction();
+  }
 };
 
 export default ContactInfoCard;
