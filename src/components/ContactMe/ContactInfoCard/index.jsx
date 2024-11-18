fdaf178bc7880c389ebe0ab9405f020507251db9
Edgar <155146007+edgar-front@users.noreply.github.com>
2024-11-18 15:18:45 +0400

    uptade refactor

--- src/components/ContactMe/ContactInfoCard/index.jsx
+++ src/components/ContactMe/ContactInfoCard/index.jsx
@@ -1,43 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Text from "../../Text";
 import Icon from "../../Icon";
 
 const ContactInfoCard = ({ text, name, link, linkLabel, showCopy }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
   const handleCopy = () => {
     navigator.clipboard
       .writeText("edgar.itmember@gmail.com")
       .then(() => {
-        console.log("Text copied to clipboard");
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000); // Hide message after 2 seconds
       })
       .catch((err) => {
         console.error("Failed to copy text: ", err);
       });
   };
+
   return (
     <div className="contact-details-card">
-      <Icon
-        alt={text}
-        className={"email"}
-        size={64}
-        color="white"
-        name={name}
-      />
-
+      <Icon alt={text} className="email" size={64} color="white" name={name} />
       <button className="copy-button" onClick={handleCopy}>
         {showCopy && (
           <Icon
             alt={text}
-            className={"copy"}
+            className="copy"
             size={32}
             color="white"
-            name={"copy-icon"}
+            name="copy-icon"
           />
         )}
       </button>
-
+      {isCopied && <p className="copy-message">Text Copied!</p>}{" "}
+      {/* Display message */}
       <Text>{text}</Text>
-
       <a target="_blank" rel="noreferrer" href={link}>
         {linkLabel}
       </a>
